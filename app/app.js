6456e976924e56147f694fd042e8e2499cbe3d21
katslump <kat.slump@gmail.com>
2018-02-17 19:13:08 -0800

    Todo items and buttons added

--- app/app.js
+++ app/app.js
@@ -1,4 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React is live!</p>, document.getElementById('root'));
+let dummyData = ["Clean my room", "Review ES6", "Call mom"];
+let key = 0;
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: dummyData
+    };
+  }
+
+  render() {
+    return (<div>
+      <ul>
+        {this.state.items.map((item) => <Todo key={key++} task={item}/>)}
+      </ul>
+    </div>);
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (<div>
+      <li><input type="submit" value="X"/>{this.props.task}</li>
+    </div>);
+  }
+}
+
+ReactDOM.render(<TodoList/>, document.getElementById('root'));

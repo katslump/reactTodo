cd0ff5a8e2c3ed762e557b48cf9be37e7a4d7c1e
katslump <kat.slump@gmail.com>
2018-02-18 12:34:02 -0700

    Todo list hooked up to mongodb with routes

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-  res.send('Hello');
+var app = express();
+
+
+var TodoItem = require('../models/TodoItem.js');
+
+app.get('/add', (req, res) => {
+
+    const testTodo = new TodoItem({
+        task: "test task",
+        completed: false
+    });
+
+    testTodo.save()
+      .then(response => {
+        res.send(response);
+      })
+      .catch(error => {
+        res.send(error);
+      })
 });
 
-module.exports = router; 
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+var fs = require('fs');
 const app = express();
+var mongoose = require('mongoose');
 const dbRoutes = require('./routes/databaseAccess.js');
 
+var TodoItem = require('./models/TodoItem.js').Todo;
+
+if (! fs.existsSync('./env.sh')) {
+  throw new Error('env.sh file is missing');
+}
+if (! process.env.MONGODB_URI) {
+  throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+}
+mongoose.connection.on('connected', function() {
+  console.log('Success: connected to MongoDb!');
+});
+mongoose.connection.on('error', function() {
+  console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
+  process.exit(1);
+});
+mongoose.connect(process.env.MONGODB_URI);
 
 // This line makes the build folder publicly available.
 app.use(express.static('build'));
 app.use('/db', dbRoutes);
+
 
 app.listen(3000, () => {
   console.log('Server for React Todo App listening on port 3000!')
